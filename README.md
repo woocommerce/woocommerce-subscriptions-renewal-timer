a7f6cf8f368a4a07289a0f7b2d75c04d379c5494
Brent Shepherd <thenbrent@gmail.com>
2016-05-09 15:26:43 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# woocommerce-subscriptions-renewal-logger
+# WooCommerce Subscriptions Renewal Logger
+
 Log the beginning and end of subscription renewal events to keep an eye on how long they are taking.
---
+++ woocommerce-subscriptions-renewal-logger.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Subscriptions Renewal Logger
+ * Plugin URI:
+ * Description: Log the beginning and end of subscription renewal events to keep an eye on how long they are taking.
+ * Author: Prospress Inc.
+ * Author URI: http://prospress.com/
+ * Version: 1.0
+ *
+ * Copyright 2016 Prospress, Inc.  (email : freedoms@prospress.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class WCS_Renewal_Logger {
+
+	private $logger;
+
+	private $process_id;
+
+	private $counter = 1;
+
+	public function __construct() {
+
+		$this->process_id = getmypid();
+
+		add_action( 'woocommerce_loaded', array( $this, 'load_logger' ) );
+
+		add_action( 'woocommerce_scheduled_subscription_payment', array( $this, 'log_renewal_start' ), 0 );
+
+		add_action( 'woocommerce_scheduled_subscription_payment', array( $this, 'log_renewal_order_created' ), 2 );
+
+		add_action( 'woocommerce_scheduled_subscription_payment', array( $this, 'log_renewal_payment_processed' ), 11 );
+	}
+
+	public function log_renewal_start( $subscription_id ) {
+		$this->log( sprintf( 'Subscription %d renewal begins.', $subscription_id ) );
+	}
+
+	public function log_renewal_order_created( $subscription_id ) {
+		$this->log( sprintf( 'Subscription %d renewal order creation completed.', $subscription_id ) );
+	}
+
+	public function log_renewal_payment_processed( $subscription_id ) {
+		$this->log( sprintf( 'Subscription %d renewal payment gateway hook completed.', $subscription_id ) );
+		$this->counter++;
+	}
+
+	/**
+	 * Wrapper function around WC_Logger->log to always use our special log file name
+	 * and prefix all log entries with the process ID to match up different queues of
+	 * renewals being processed.
+	 *
+	 * @param string $message Message to log
+	 */
+	protected function log( $message ) {
+		$log_message = sprintf( 'PID %d: Renewal %d. %s', $this->process_id, $this->counter, $message );
+		$this->logger->add( 'wcs-renewal', $log_message );
+		error_log( $log_message );
+	}
+
+	/**
+	 * Sets up the logging class
+	 */
+	public function load_logger() {
+		$this->logger = new WC_Logger();
+	}
+
+}
+new WCS_Renewal_Logger();
